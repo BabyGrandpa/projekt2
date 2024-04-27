ecbea7ae26e514f5923f27cdb6c6fbbacab48a4e
Emilba123 <emilba2000@live.dk>
2024-04-27 21:40:07 +0200

    I dag gik det godt

--- Projekt2.py
+++ Projekt2.py
@@ -1,7 +1,47 @@
-# importing pandas module 
-import pandas as pd 
-import serial #bruges til at kommunikere med uart 
-import serial.tools.list_ports #bruges til at kommunikere med uart 
-from datetime import datetime
-from datetime import date
+import pandas as pd
+import serial
+import serial.tools.list_ports
+from datetime import datetime, date
 import numpy as np
+
+
+#Ved hjælp af self kan du 
+#tilgå og manipulere disse værdier individuelt for hver instans.
+class Tid_Dato:
+    def __init__(self):
+        self.nuvaerende_dato = date.today() 
+        self.nuvaerende_tid = self.get_current_time()
+
+    @staticmethod 
+    def get_current_time():
+        now = datetime.now()
+        time_without_seconds = now.strftime('%H:%M')
+        return time_without_seconds
+    
+    
+
+    def udskriv_data(self):
+        print(self.nuvaerende_dato, self.nuvaerende_tid)
+
+    def CSV(self, filnavn):
+        #nøgle er "Dato", der repræsentere kolonne overskrift: 
+        #[self.nuvarende_dato/tid] er en liste der indeholder en værdi
+        data = {
+        "Dato": [self.nuvaerende_dato],
+        "Tid": [self.nuvaerende_tid],   
+    }
+        dataframe = pd.DataFrame(data)
+
+        #lav dataframen til en csv-fil
+        dataframe.to_csv(filnavn, index = False, encoding='utf-8')
+        #index=False for ikke at inkludere rækkeindex i filen
+    
+S1=Tid_Dato()
+S1.udskriv_data()
+S1.CSV("Tid_Dato")
+
+   
+
+    
+
+
--- hello.py
+++ hello.py
@@ -1,20 +1,38 @@
 
 # importing pandas module 
 import pandas as pd 
-  
-# making data frame 
-data = pd.read_csv("https://media.geeksforgeeks.org/wp-content/uploads/nba.csv") 
-  
-# number of rows to return 
-n = 9
-  
-# creating series 
-series = data["Name"] 
-  
-# returning top n rows 
-top = series.head(n = n) 
-  
-# display 
-top 
+import serial #bruges til at kommunikere med uart 
+import serial.tools.list_ports #bruges til at kommunikere med uart 
+from datetime import datetime
+from datetime import date
+import numpy as np
 
-print("din moder")
+def get_current_time():
+    now = datetime.now()
+    time_without_seconds = now.strftime('%H:%M')
+    return time_without_seconds
+
+#Variable 
+Motor = False
+nuvaerende_dato = date.today()
+nuvaerende_tid = get_current_time()
+
+
+
+# making data frame 
+data = pd.read_csv("nba.csv") #læser csv filen
+print(data.head()) #printer top 5 lines i csv filen
+print(data.tail())#printer bund 5 lines i csv filen
+
+def StartMotor():
+    Motor = True
+
+print("dato:", nuvaerende_dato)
+print("tid:", nuvaerende_tid)
+arr = np.array([nuvaerende_dato, nuvaerende_tid])
+print("Array:", "\n", arr)
+
+
+
+
+
