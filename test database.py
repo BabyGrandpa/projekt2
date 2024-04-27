951b6bc90b86edb939c659273fac22126561421a
BabyGrandpa <rafanulet@msn.com>
2024-04-27 21:43:41 +0200

    update

--- test database.py
+++ test database.py
@@ -1,23 +1,39 @@
 import pandas as pd
 import numpy as np
+from csv import DictWriter
 
 class SensorData:
     temp = [20,21,23,21,25,11,14,13,17]
     wind = [4,5,6,10,1,3,2,5,10]
     time = ['16:10','16:15','16:20','16:25','16:30','16:35','16:40','16:45','16:50']
     date = ['27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024']
+    datas = ['temperatur', 'wind speed', 'current time', 'current date']
 
     dict = {'temperatur': temp, 'wind speed': wind, 'current time': time, 'current date':date}
+
+    def add_row(Self,a,b,c,d):
+        better_dict = {'temperatur': a, 'wind speed': b, 'current time': c, 'current date': d}
+
+        with open('SensorData.csv','a') as f_object:
+            this = DictWriter(f_object, fieldnames=something.datas)
+            this.writerow(better_dict)
+            f_object.close()
 
 something = SensorData()
 
 df = pd.DataFrame(something.dict)
-df.to_csv('SensorData.csv')
+df.to_csv('SensorData.csv', index=False)
+
+a = 17
+b = 10
+c = '16:55'
+d = '27/04/2024'
+
+something.add_row(a,b,c,d)
+
 
 dread = pd.read_csv("SensorData.csv",header = 0, usecols=["temperatur","wind speed","current time", "current date"])
 
-gk = dread.groupby('current date')
-for date,group in gk:
-    print(date)
-    print(group)
-    print()
+gk = dread.groupby('wind speed')
+gr = gk.get_group(10)
+print(gr)
