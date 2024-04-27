55f7258e7c335d3faadc6a01bc4bc3f8cefe5ca0
Emilba123 <emilba2000@live.dk>
2024-04-27 16:07:20 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,10 +3,21 @@
 import pandas as pd 
 import serial #bruges til at kommunikere med uart 
 import serial.tools.list_ports #bruges til at kommunikere med uart 
+from datetime import datetime
 
+#Variable 
+Motor = False
+nuvaerende_dato_tid = datetime.now()
 
 # making data frame 
 data = pd.read_csv("nba.csv") #læser csv filen
 print(data.head()) #printer top 5 lines i csv filen
+print(data.tail())#printer bund 5 lines i csv filen
 
-  
+
+
+def StartMotor():
+    Motor = True
+print(nuvaerende_dato_tid)
+
+
