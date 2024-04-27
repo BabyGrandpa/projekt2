ee96ac8e96958641608a15ab69315a7aa78918a4
Emilba123 <emilba2000@live.dk>
2024-04-27 20:34:57 +0200

    Create Projekt2.py

---
+++ Projekt2.py
@@ -0,0 +1,7 @@
+# importing pandas module 
+import pandas as pd 
+import serial #bruges til at kommunikere med uart 
+import serial.tools.list_ports #bruges til at kommunikere med uart 
+from datetime import datetime
+from datetime import date
+import numpy as np
