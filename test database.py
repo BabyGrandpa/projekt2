82df724dc7b2416f6a881ea54e74fde512895a32
BabyGrandpa <rafanulet@msn.com>
2024-04-30 11:06:24 +0200

    YES GUDDES SIIIRR!

--- test database.py
+++ test database.py
@@ -8,40 +8,46 @@ class SensorData:
     wind = [4,5,6,10,1,3,2,5,10]
     time = ['16:10','16:15','16:20','16:25','16:30','16:35','16:40','16:45','16:50']
     date = ['27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024']
-    datas = ['temperatur', 'wind speed', 'current time', 'current date']
+    datas = ['temperatur', 'wind speed', 'current time', 'current date', 'within threshold']
 
-    dict = {'temperatur': temp, 'wind speed': wind, 'current time': time, 'current date':date}
+    def __init__(self):
+        dict = {'temperatur': self.temp, 'wind speed': self.wind, 'current time': self.time, 'current date': self.date, 'within threshold': bool}
+        df = pd.DataFrame(dict)
+        df.to_csv('SensorData.csv', index=False)
 
     def add_row(self, a, b, c, d):
-        better_dict = {'temperatur': a, 'wind speed': b, 'current time': c, 'current date': d}
+        better_dict = {'temperatur': a, 'wind speed': b, 'current time': c, 'current date': d,'within threshold': self.within_tresh(b,6,5)}
 
         with open('SensorData.csv', 'a', newline='') as f_object:
             dict_writer = DictWriter(f_object, fieldnames=self.datas)
             dict_writer.writerow(better_dict)
     
-    def get_date(Self):
+    def get_date(self):
         date = dt.date.today()
         date_c = date.strftime('%d/%m/%Y')
         return date_c
         
-    def get_time(Self):
+    def get_time(self):
         time = dt.datetime.now()
         time_c = time.strftime('%H:%M')
         return time_c
+    
+    def within_tresh(self,a,b,c):
+        if (a <= b and a >= c ):
+            return True
+        return False
 
 something = SensorData()
-df = pd.DataFrame(something.dict)
-#df.to_csv('SensorData.csv', index=False)
 
 a = 17
-b = 10
+b = 5
 c = something.get_time()
 d = something.get_date()
 
 something.add_row(a,b,c,d)
 
-dread = pd.read_csv("SensorData.csv", header = 0, usecols=["temperatur","wind speed","current time", "current date"])
+dread = pd.read_csv("SensorData.csv", header = 0, usecols=["temperatur","wind speed","current time", "current date", "within threshold"])
 
 gk = dread.groupby('wind speed')
-gr = gk.get_group(10)
+gr = gk.get_group(5)
 print(gr)
