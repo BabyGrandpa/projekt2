aacbdae14a61ee7ac88298cc4695b083df707df1
BabyGrandpa <rafanulet@msn.com>
2024-06-02 16:31:07 +0200

    yes

--- datafind.py
+++ datafind.py
@@ -1,8 +1,30 @@
-import pandas as pd
+import datetime as dt
 
+#returnere dato med format eksemple 1
+def get_date1():
+    date = dt.date.today()
+    date_c = date.strftime('%d/%m/%Y')
+    return date_c
 
-dread = pd.read_csv("SensorData.csv", header = 0, usecols=["temperatur","wind speed","current time", "current date", "within threshold"])
+#returnere dato med format eksemple 2
+def get_date2():
+    date = dt.date.today()
+    date_c = date.strftime('%d/%m/%y')
+    return date_c
 
-gk = dread.groupby('within threshold')
-gr = gk.get_group('True')
-print(gr)
+#returnere tid med format eksemple 1
+def get_time1():
+    time = dt.datetime.now()
+    time_c = time.strftime('%H:%M')
+    return time_c
+
+#returnere tid med format eksemple 2
+def get_time2():
+    time = dt.datetime.now()
+    time_c = time.strftime('%h:%m')
+    return time_c
+
+print("Date format example 1",get_date1())
+print("Date format example 2",get_date2())
+print("Time format example 1",get_time1())
+print("Time format example 2",get_time2())
--- test database.py
+++ test database.py
@@ -3,55 +3,120 @@ import numpy as np
 from csv import DictWriter
 import datetime as dt
 import time
+import os
+from flask import Flask, request, jsonify
+import serial.tools.list_ports
 
 class SensorData:
+    #Test data (se test_data funktionen)
     temp = [20,21,23,21,25,11,14,13,17]
     wind = [4,5,6,10,1,3,2,5,10]
     time = ['16:10','16:15','16:20','16:25','16:30','16:35','16:40','16:45','16:50']
     date = ['27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024','27/04/2024']
-    datas = ['temperatur', 'wind speed', 'current time', 'current date', 'within threshold']
+    datas = ['temperatur', 'wind speed', 'current time', 'current date', 'Predicted power']
 
+    #constructor, opretter csv-fil. 
     def __init__(self):
-        dict = {'temperatur': self.temp, 'wind speed': self.wind, 'current time': self.time, 'current date': self.date, 'within threshold': bool}
-        df = pd.DataFrame(dict)
-        df.to_csv('SensorData.csv', index=False)
+        self.file_path = 'SensorData.csv'
+        self.columns = ['temperatur', 'wind speed', 'current time', 'current date', 'Predicted power']
+        if not os.path.exists(self.file_path):
+            df = pd.DataFrame(columns=self.columns)
+            df.to_csv(self.file_path, index=False)  
 
-    def add_row(self, a, b, c, d):
-        better_dict = {'temperatur': a, 'wind speed': b, 'current time': c, 'current date': d,'within threshold': self.within_tresh(b,6,5)}
+    # dette er dataen der bliver gemt i csv filen
+    def add_row(self, temp, wind, time, date, effect):
 
-        with open('SensorData.csv', 'a', newline='') as f_object:
-            dict_writer = DictWriter(f_object, fieldnames=self.datas)
-            dict_writer.writerow(better_dict)
+        new_row = pd.DataFrame([[temp, wind, time, date, effect]], columns=self.columns)
+        df = pd.read_csv(self.file_path)
+        df = pd.concat([df, new_row], ignore_index=True)
+        df.to_csv(self.file_path, index=False)
+        
+
+    #tilføjer test data
+    def add_test_data(self):
+        for i in range(8):
+            self.add_row(self.temp[i], self.wind[i], self.time[i], self.date[i], 1)
     
+    #returnere dato
     def get_date(self):
         date = dt.date.today()
         date_c = date.strftime('%d/%m/%Y')
         return date_c
-        
+    
+    #returnere tid i timer og minutter
     def get_time(self):
         time = dt.datetime.now()
         time_c = time.strftime('%H:%M')
         return time_c
     
-    def within_tresh(self, a, b, c):
-        if (a <= b and a >= c ):
-            return True
-        return False
+    # vindmøllens teoretiske effekt ud fra gennemsnitlig typisk industrivindmølle-værdier.  
+    def effect(self,wind):
+        #luftens densitet (kg/m^3):
+        p = 1.225
+        #Radius af rotorblade (meter):
+        R = 20
+        #gennemsnitlig vindhasstighed (m/s): 
+        v = wind
+        #effektkoefficient, (maks 0.59 (Betz'lov))
+        Cp = 0.59
+        effect = 1/2*Cp*p*(R**2*(np.pi))*v**3
+        return effect
+    
+    #returnere seneste datamåling
+    def get_latest(self):
+        df = pd.read_csv('SensorData.csv')
+        return df.tail(1)
+    
+    #returnere seneste 10 datamålinger
+    def get_10(self):
+        df = pd.read_csv('SensorData.csv')
+        return df.tail(10)
+    
+    #returnere seneste 100 datamålinger
+    def get_100(self):
+        df = pd.read_csv('SensorData.csv')
+        return df.tail(100)  
+    
+    #def UART(self):
+        ser = serial.Serial(port='COM10',\
+                            baudrate=57600,\
+                            parity=serial.PARITY_NONE,\
+                            stopbits=serial.STOPBITS_ONE,\
+                            bytesize=serial.EIGHTBITS,\
+                            timeout=0)
 
-something = SensorData()
+        data_l = []
 
-while 1:
-    a = np.random.randint(10,25)
-    b = np.random.randint(1,14)
-    c = something.get_time()
-    d = something.get_date()
+        while True:
+            # Læser alle indgående bytes
+            for packet in ser.read():
+                # Alle de læste bytes bliver lagret i data_l listen, og bliver converteret ind til char variabler
+                data_l.append(chr(packet))
+                # Hvis den læste variable sendt fra PSoC er et S, starter convertions processen
+                if chr(packet) == 'S':
+                    # Alle elementer i data_l bliver sadt ind i variablen data, og konvertere alle char til en string
+                    data = ''.join([str(data) for data in data_l])
+                    # Der printes data hvorefter stringen bliver splittet alle stederne med ", " hvor førte element efter ", " er vind og anden element er temperatur
+                    print("\nData:", data)
+                    wind = data.split(", ")[1]
+                    temp = data.split(", ")[2]
+                    print("wind:", wind)
+                    print("temp:", temp)
+                    # Den oversatte sensordata bliver passeret gennem add_row() funktionen som en float istedet for en string, 
+                    # og variblerne data_l og data bliver tømt så det er klar til næste læsning
+                    self.add_row(float(temp),float(wind), self.get_time(), self.get_date(), self.effect(wind))
+                    data_l = []
+                    data = "" 
+        ser.close()
+        
+    
+Sensor = SensorData()
 
-    something.add_row(a,b,c,d)
-    time.sleep(5)
+Sensor.add_test_data()
+Sensor.add_row(1,1,Sensor.get_time(),Sensor.get_date(),1)
+#print(Sensor.effect(), "watt")
+#print(Sensor.get_latest())
+#print(Sensor.get_last10temp())
 
 
-dread = pd.read_csv("SensorData.csv", header = 0, usecols=["temperatur","wind speed","current time", "current date", "within threshold"])
 
-gk = dread.groupby('within threshold')
-gr = gk.get_group(True)
-print(gr)
